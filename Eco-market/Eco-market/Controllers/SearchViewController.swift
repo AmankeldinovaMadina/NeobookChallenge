c02338dbfbcd50b1e8606d714af8b19a14da51fd
Madina <madinaamankeldinova5@gmail.com>
2023-12-22 22:41:14 +0600

    added title in the search view

--- Eco-market/Eco-market/Controllers/SearchViewController.swift
+++ Eco-market/Eco-market/Controllers/SearchViewController.swift
@@ -1,10 +1,6 @@
 import Foundation
 import UIKit
 
-enum SelectedCategory{
-    
-}
-
 class SearchViewController: UIViewController, UISearchBarDelegate {
     
     var selectedCategoryIndex: Int = 0
@@ -18,22 +14,33 @@ class SearchViewController: UIViewController, UISearchBarDelegate {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupUI()
-        
-
+        customizeBackButton()
     }
 
     private func setupUI() {
         // Navigation Bar Setup
-        navigationItem.title = "Продукты"
-        navigationController?.navigationBar.prefersLargeTitles = true
+        let searchViewTitle = UILabel()
+        searchViewTitle.text = "Продукты"
+        searchViewTitle.textColor = .black
+        searchViewTitle.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        navigationItem.titleView = searchViewTitle
         
         // Search Bar Setup
         searchBar.delegate = self
         searchBar.placeholder = "Быстрый поиск"
-        searchBar.searchTextField.textColor = .white
-        searchBar.showsCancelButton = true
+        searchBar.searchTextField.textColor = .black
         
-        navigationItem.titleView = searchBar
+        searchBar.showsCancelButton = false
+        
+        
+        view.addSubview(searchBar)
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10)
+        ])
 
         // Segmented Control Setup
         segmentedControl.selectedSegmentIndex = 1
@@ -43,18 +50,19 @@ class SearchViewController: UIViewController, UISearchBarDelegate {
         view.addSubview(segmentedControl)
         segmentedControl.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            segmentedControl.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 10),
             segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
             segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10)
         ])
 
         // Empty State Setup
-        emptyStateImageView.image = UIImage(named: "empty") // Replace with your empty state image
+        emptyStateImageView.image = UIImage(named: "empty")
         emptyStateImageView.contentMode = .scaleAspectFit
 
         emptyStateLabel.text = "Ничего не нашли"
         emptyStateLabel.textAlignment = .center
         emptyStateLabel.font = UIFont.systemFont(ofSize: 16)
+        emptyStateLabel.textColor = UIColor(red: 0.673, green: 0.671, blue: 0.679, alpha: 1)
 
         // Layout Empty State Views
         let stackView = UIStackView(arrangedSubviews: [emptyStateImageView, emptyStateLabel])
@@ -73,7 +81,36 @@ class SearchViewController: UIViewController, UISearchBarDelegate {
             emptyStateImageView.heightAnchor.constraint(equalToConstant: 200) // Adjust height as necessary
         ])
     }
+    
+    private func customizeBackButton() {
+        // Create the back button image
+            let backImage = UIImage(systemName: "chevron.left")?.withRenderingMode(.alwaysTemplate)
+        
+            // Create a button and set the image for the button
+            let backButton = UIButton(type: .system)
+            backButton.setImage(backImage, for: .normal)
+            backButton.tintColor = .black // Set your color
+        
+            backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+            
+            // Size the button to fit the content
+            backButton.sizeToFit()
+            
+            // Create a UIBarButtonItem using the custom button
+            let backButtonItem = UIBarButtonItem(customView: backButton)
+            
+            // Assign the custom UIBarButtonItem as the leftBarButtonItem
+            navigationItem.leftBarButtonItem = backButtonItem
+            
+            // If you want to remove the default navigation back button title, set it to an empty string
+            navigationItem.backButtonTitle = ""
+    }
 
+    @objc private func backButtonTapped() {
+        // Action to perform when the custom back button is tapped
+        navigationController?.popViewController(animated: true)
+    }
+    
     @objc private func segmentChanged(_ sender: UISegmentedControl) {
         // Handle segment change
     }
---
+++ Eco-market/Eco-market/Views/CustomProductCollectionViewCell.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+class CustomProductCollectionViewCell: UICollectionViewCell {
+   static let identifier = "CustomProductCollectionViewCell"
+    
+    private let image: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFill
+        image.image = UIImage(systemName: "questionmark")
+        image.tintColor = .white
+        image.clipsToBounds = true
+        return image
+    }()
+    
+//    internal let description: UILabel = {
+//        let description = UILabel()
+//        description.textColor = UIColor.black
+//        description.numberOfLines = 0
+//        
+//    }()
+    
+    private let price: UILabel = {
+        let price = UILabel()
+        price.textColor = UIColor(red: 0.459, green: 0.859, blue: 0.106, alpha: 1)
+        price.font = UIFont(name: "SFPro-Bold", size: 20)
+        
+        return price
+    }()
+    
+    private let buttonAdd: UIButton = {
+        let buttonAdd = UIButton()
+        
+    }()
+    
+}
